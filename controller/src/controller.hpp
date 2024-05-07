8e2087220d1a0596a6b0d64d8f6e921e400ac283
syoch <syoch64@gmail.com>
2024-05-07 11:04:28 +0000

    Update: Setupped controller code

---
+++ controller/src/controller.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <mbed.h>
+
+#include <robotics/node/node.hpp>
+
+namespace nhk2024b {
+class ControllerReader {
+  template <typename T>
+  using Node = robotics::Node<T>;
+
+  Thread *thread;
+
+  // ここをいじる
+  mbed::AnalogIn value_1_in;
+
+ public:
+  // ここをいじる
+  Node<float> value_1;
+
+  ControllerReader() : value_1_in(PA_3) {
+    thread = new Thread();
+    thread->start([this]() { ControllerThread(); });
+  }
+
+  void ControllerThread() {
+    while (1) {
+      // ここをいじる
+      value_1.SetValue(value_1_in.read());
+
+      thread_sleep_until(5);  // 5ms
+    }
+  }
+};
+}  // namespace nhk2024b
