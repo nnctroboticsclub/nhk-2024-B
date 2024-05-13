ea8bedcc460c2779b0a6ce4817d3f47ff7946fb5
timniku <myonniku@gmail.com>
2024-05-13 08:05:24 +0000

    add time fanction

--- robot-bridge/src/bridge.hpp
+++ robot-bridge/src/bridge.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <robotics/controller/float.hpp>
 #include <robotics/controller/action.hpp>
+#include <robotics/controller/float.hpp>
 #include <robotics/filter/inc_angled_motor.hpp>
 
 namespace nhk2024b {
@@ -14,20 +14,22 @@ class BridgeController {
   };
 
   // どういうノードをコントローラーから生やすか
-  controller::Float rotate;
+  controller::Action rotate;
   controller::Action button;
-  
+
   // const はコンストラクタないで変更がないことを明記/強制
-  BridgeController(const Config& config):
-    rotate(config.rotate_id),
-    button(config.button_id)
-  {
-  }
+  BridgeController(const Config &config)
+      : rotate(config.rotate_id), button(config.button_id) {}
 };
 
 class Bridge {
-  template<typename T>
+  template <typename T>
   using Node = robotics::Node<T>;
+
+  const std::chrono::duration<float> revolver_reversing_time = 30ms;
+
+  enum class LoadState { Start_rote };
+  LoadState state;
 
   // コントローラー Node をまとめて持つ
   BridgeController ctrl;
@@ -38,19 +40,26 @@ class Bridge {
   using IncAngledMotor = robotics::filter::IncAngledMotor<float>;
   IncAngledMotor lock;
 
-  Bridge(BridgeController::Config &ctrl_config): ctrl(ctrl_config) {
-  }
+  Bridge(BridgeController::Config &ctrl_config) : ctrl(ctrl_config) {}
 
   void LinkController() {
     ctrl.rotate.Link(out_a);
-
-    ctrl.button.OnFire([this](){//押された時の処理
+    ctrl.button.OnFire([this]() {  // 押された時の処理
       lock.AddAngle(180);
+    });
+
+    ctrl.rotate.OnFire([this]() {  // 押された時の処理
+      switch (state) {
+        case LoadState::kInReversing:
+          if (timer.elapsed_time() > revolver_reversing_time) {
+            // revolver_reversing_timeは定数
+            // timer.elapsed()は関数
+            SetLoadState(LoadAction::kStopCounterRotation);
+          }
+      }
     });
   }
 
-  void Update(float dt){
-    lock.Update(dt);
-  }
+  void Update(float dt) { lock.Update(dt); }
 };
-} // namespace nhk2024b
+}  // namespace nhk2024b
