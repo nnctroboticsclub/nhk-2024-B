5fbbaa3b81d6f28fa0ab2a9313e02f662e26a6a2
syoch <syoch64@gmail.com>
2024-09-03 11:20:48 +0000

    Update: Added double free checker

--- common_libs/nhk2023-b/cxx/mem.cpp
+++ common_libs/nhk2023-b/cxx/mem.cpp
@@ -1,7 +1,36 @@
 #include "nhk2024b/ffi_mem.hpp"
 
 #include <cstdlib>
+#include <stdexcept>
+#include <unordered_set>
 
-void* __ffi_malloc(size_t size) { return malloc(size); }
+#include <robotics/logger/logger.hpp>
+#include <robotics/platform/thread.hpp>
 
-void __ffi_free(void* ptr) { free(ptr); }
+static robotics::logger::Logger logger{"system.ffi", "FFISystem"};
+
+static std::unordered_set<uint32_t> allocated_pointer;
+
+void* __ffi_malloc(size_t size) {
+  auto ptr = malloc(size);
+  logger.Info("malloc(%d) => %p", size, ptr);
+
+  allocated_pointer.emplace((uint32_t)ptr);
+
+  return ptr;
+}
+
+void __ffi_free(void* ptr) {
+  if (allocated_pointer.find((uint32_t)ptr) == allocated_pointer.end()) {
+    logger.Error("Double free detected on %p\n", ptr);
+
+    while(1) {
+      using namespace std::chrono_literals;
+      robotics::system::SleepFor(1s);
+    }
+  }
+  allocated_pointer.erase((uint32_t)ptr);
+
+  logger.Info("free(%p)", ptr);
+  free(ptr);
+}
