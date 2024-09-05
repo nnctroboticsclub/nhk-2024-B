53f4bdd45f4ed3a5087f6be3ddb8ab81b76fa07b
timniku <myonniku@gmail.com>
2024-09-05 10:24:39 +0000

    Update: Changed moving component's formula

--- robot1-main/src/robot1-main.hpp
+++ robot1-main/src/robot1-main.hpp
@@ -33,11 +33,11 @@ class Refrige {
   void LinkController() {
     ctrl_move.SetChangeCallback([this](robotics::JoyStick2D stick) {
       double setmotor[4]={-M_PI/4,M_PI/4,3*M_PI/4,5*M_PI/4};
-      out_motor1.SetValue((cos(setmotor[0])*stick[1] + sin(setmotor[0])*stick[0])*0.7);
-      out_motor2.SetValue((cos(setmotor[1])*stick[1] + sin(setmotor[1])*stick[0])*0.7);
-      out_motor3.SetValue((cos(setmotor[2])*stick[1] + sin(setmotor[2])*stick[0])*0.7);
-      out_motor4.SetValue((cos(setmotor[3])*stick[1] + sin(setmotor[3])*stick[0])*0.7);
-
+      
+      out_motor1.SetValue((cos(setmotor[0])*stick[0] + sin(setmotor[0])*stick[1])*0.7 * -1);
+      out_motor2.SetValue((cos(setmotor[1])*stick[0] + sin(setmotor[1])*stick[1])*0.7 * -1);
+      out_motor3.SetValue((cos(setmotor[2])*stick[0] + sin(setmotor[2])*stick[1])*0.7 * -1);
+      out_motor4.SetValue((cos(setmotor[3])*stick[0] + sin(setmotor[3])*stick[1])*0.7 * -1);
     });
     // ↓ボタンの作動
     ctrl_unlock.SetChangeCallback(//ロック解除
@@ -52,17 +52,17 @@ class Refrige {
 
     ctrl_collector.SetChangeCallback(
         [this](bool btn) {  // 回収機構押しているときだけ正転
-          out_collector.SetValue(btn ? 0.4 : 0);
+          out_collector.SetValue(btn ? 0.5 : 0);
         });
 
     ctrl_brake.SetChangeCallback(
         [this](bool btn) {  // ブレーキ　ボタン押しているときだけ逆回転
-          out_brake.SetValue(btn ? -0.4 : 0);
+          out_brake.SetValue(btn ? -0.9 : 0);
         });
     
     ctrl_brake_back.SetChangeCallback(//ブレーキ逆向き
         [this](bool btn) {
-          out_brake.SetValue(btn ?   0.4 : 0);
+          out_brake.SetValue(btn ?   0.9 : 0);
         });
   }
 };
