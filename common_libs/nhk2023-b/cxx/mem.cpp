48161a1cf1a687f2cb7616e1351fcc2df87cc775
syoch <syoch64@gmail.com>
2024-09-05 00:25:08 +0000

    Update: comment out memory allocation log

--- common_libs/nhk2023-b/cxx/mem.cpp
+++ common_libs/nhk2023-b/cxx/mem.cpp
@@ -13,7 +13,7 @@ static std::unordered_set<uint32_t> allocated_pointer;
 
 void* __ffi_malloc(size_t size) {
   auto ptr = malloc(size);
-  logger.Info("malloc(%d) => %p", size, ptr);
+  // logger.Info("malloc(%d) => %p", size, ptr);
 
   allocated_pointer.emplace((uint32_t)ptr);
 
@@ -24,13 +24,13 @@ void __ffi_free(void* ptr) {
   if (allocated_pointer.find((uint32_t)ptr) == allocated_pointer.end()) {
     logger.Error("Double free detected on %p\n", ptr);
 
-    while(1) {
+    while (1) {
       using namespace std::chrono_literals;
       robotics::system::SleepFor(1s);
     }
   }
   allocated_pointer.erase((uint32_t)ptr);
 
-  logger.Info("free(%p)", ptr);
+  // logger.Info("free(%p)", ptr);
   free(ptr);
 }
