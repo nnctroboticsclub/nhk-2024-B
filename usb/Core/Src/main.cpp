6900064f752fb996efc4e62319e175b0391fe842
syoch <syoch64@gmail.com>
2024-09-08 14:49:26 +0000

    Update: Integrated IM920_rs

--- usb/Core/Src/main.cpp
+++ usb/Core/Src/main.cpp
@@ -24,6 +24,9 @@
 /* USER CODE BEGIN Includes */
 #include "usbh_hid.h"
 #include <stdio.h>
+#include <srobo2/ffi/base.hpp>
+#include <srobo2/com/im920.hpp>
+#include <chrono>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -43,16 +46,18 @@
 
 /* Private variables ---------------------------------------------------------*/
 UART_HandleTypeDef huart2;
+UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
+static void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_USART1_UART_Init(void);
 static void MX_USART2_UART_Init(void);
-void MX_USB_HOST_Process(void);
+extern "C" void MX_USB_HOST_Process(void);
 
 /* USER CODE BEGIN PFP */
 
@@ -61,15 +66,148 @@ void MX_USB_HOST_Process(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-extern void initialise_monitor_handles(void);
+namespace robotics::system {
+void SleepFor(std::chrono::milliseconds duration) {
+  HAL_Delay(duration.count());
+}
+}  // namespace robotics::system
 
+namespace nhk2024b::controller {
+
+namespace im920 {
+robotics::logger::Logger im920_logger("im920.app", "Com:IM920");
+
+class IM920TxCStream {
+  srobo2::ffi::CStreamTx tx;
+  bool is_sending = false;
+
+  static void Write(const void *instance, const void *context,
+                    const uint8_t *data, size_t len) {
+    auto self = const_cast<IM920TxCStream *>(
+        reinterpret_cast<const IM920TxCStream *>(instance));
+    self->DoWrite(data, len);
+  }
+
+  void DoWrite(const uint8_t *data, size_t len) {
+    im920_logger.Info("IM920 <-- ");
+    im920_logger.Hex(robotics::logger::core::Level::kInfo, data, len);
+
+    HAL_UART_Transmit(&huart1, const_cast<uint8_t *>(data), len, 1000);
+  }
+
+  static IM920TxCStream instance;
+
+ public:
+  static IM920TxCStream *GetInstance() { return &instance; }
+
+  void Init() { srobo2::ffi::__ffi_cstream_associate_tx(&tx, this, &Write); }
+
+  srobo2::ffi::CStreamTx *GetTx() { return &tx; }
+};
+
+IM920TxCStream IM920TxCStream::instance;
+
+class IM920RxCStream {
+  srobo2::ffi::CStreamRx *rx;
+  uint8_t data[1];
+
+  static IM920RxCStream instance;
+
+ public:
+  static IM920RxCStream *GetInstance() { return &instance; }
+
+  void Init() {
+    rx = srobo2::ffi::__ffi_cstream_new_rx();
+
+    HAL_UART_Receive_IT(&huart1, data, 1);
+  }
+
+  void RxIRQ() {
+    srobo2::ffi::__ffi_cstream_feed_rx(rx, data, 1);
+    HAL_UART_Receive_IT(&huart1, data, 1);
+  }
+
+  srobo2::ffi::CStreamRx *GetRx() { return rx; }
+};
+
+IM920RxCStream IM920RxCStream::instance;
+
+class HALCTime {
+  srobo2::ffi::CTime time;
+
+  static float Now(const void *context) { return HAL_GetTick() / 1000.0f; }
+
+  static void Sleep(const void *context, float duration) {
+    HAL_Delay(duration * 1000);
+  }
+
+  static HALCTime instance;
+
+ public:
+  static HALCTime *GetInstance() { return &instance; }
+
+  void Init() {
+    srobo2::ffi::__ffi_ctime_set_context(&time, this);
+    srobo2::ffi::__ffi_ctime_set_now(&time, &Now);
+    srobo2::ffi::__ffi_ctime_set_sleep(&time, &Sleep);
+  }
+
+  srobo2::ffi::CTime *GetTime() { return &time; }
+};
+
+HALCTime HALCTime::instance;
+
+srobo2::com::CIM920 *GetIM920() {
+  static srobo2::com::CIM920 *im920 = nullptr;
+  if (im920 == nullptr) {
+    im920 = new srobo2::com::CIM920(IM920TxCStream::GetInstance()->GetTx(),
+                                    IM920RxCStream::GetInstance()->GetRx(),
+                                    HALCTime::GetInstance()->GetTime());
+  }
+
+  return im920;
+}
+
+extern "C" void USART1_IRQHandler(void) { HAL_UART_IRQHandler(&huart1); }
+
+extern "C" void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+  if (huart->Instance == USART1) {
+    IM920RxCStream::GetInstance()->RxIRQ();
+  }
+}
+}  // namespace im920
+
+robotics::logger::Logger app_logger{"app", "   App   "};
+
+void OnIM920Packet(uint16_t from, uint8_t *data, size_t len) {
+  app_logger.Info("Received from %d: ", from);
+  app_logger.Hex(robotics::logger::core::Level::kInfo, data, len);
+}
+
+void Init() {
+  auto im920_ = im920::GetIM920();
+
+  auto node_id = im920_->GetNodeNumber(1.0f);
+  app_logger.Info("Node ID: %d", node_id);
+
+  auto version = im920_->GetVersion(1.0f);
+  app_logger.Info("Version: %s", version.c_str());
+
+  im920_->OnData(&OnIM920Packet);
+
+  im920_->Send(3 - node_id, (const uint8_t *)"Hello, world!", 13, 1.0f);
+
+  app_logger.Info("Start!");
+}
+
+}  // namespace nhk2024b::controller
 /* USER CODE END 0 */
 
 /**
  * @brief  The application entry point.
  * @retval int
  */
-int main(void) {
+extern "C" int main(void) {
   /* USER CODE BEGIN 1 */
   setbuf(stdout, NULL);
   /* USER CODE END 1 */
@@ -93,17 +231,23 @@ int main(void) {
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_USB_HOST_Init();
   MX_USART2_UART_Init();
-  /* USER CODE BEGIN 2 */
+  robotics::logger::core::Init();
 
-  printf("Started!\n");
+  MX_USB_HOST_Init();
+  MX_USART1_UART_Init();
+
+  /* USER CODE BEGIN 2 */
+  printf("\n\n\nProgram started\n");
+
+  nhk2024b::controller::Init();
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1) {
+    robotics::logger::core::LoggerProcess();
     /* USER CODE END WHILE */
     MX_USB_HOST_Process();
 
@@ -116,7 +260,7 @@ int main(void) {
  * @brief System Clock Configuration
  * @retval None
  */
-void SystemClock_Config(void) {
+static void SystemClock_Config(void) {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
@@ -185,6 +329,57 @@ static void MX_USART2_UART_Init(void) {
 }
 
 /**
+ * @brief USART1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_USART1_UART_Init(void) {
+  /* USER CODE BEGIN USART1_Init 0 */
+  // nothing
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+  printf("HAL_UART_MspInit (UART1)\n");
+  /* Peripheral clock enable */
+  __HAL_RCC_USART1_CLK_ENABLE();
+
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  /**USART1 GPIO Configuration
+  PA9     ------> USART1_TX
+  PA10     ------> USART1_RX
+  */
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  GPIO_InitStruct.Pin = GPIO_PIN_9 | GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  /* USER CODE END USART1_Init 1 */
+
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 19200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK) {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  HAL_NVIC_SetPriority(USART1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(USART1_IRQn);
+
+  nhk2024b::controller::im920::IM920RxCStream::GetInstance()->Init();
+  nhk2024b::controller::im920::IM920TxCStream::GetInstance()->Init();
+
+  /* USER CODE END USART1_Init 2 */
+}
+
+/**
  * @brief GPIO Initialization Function
  * @param None
  * @retval None
@@ -203,14 +398,14 @@ static void MX_GPIO_Init(void) {
 
 /* USER CODE BEGIN 4 */
 
-void Test_syoch_01(USBH_HandleTypeDef *phost);
+extern "C" void Test_syoch_01(USBH_HandleTypeDef *phost);
 
 /**
  * @brief  The function is a callback about HID Data events
  *  @param  phost: Selected device
  * @retval None
  */
-void USBH_HID_EventCallback(USBH_HandleTypeDef *phost) {
+extern "C" void USBH_HID_EventCallback(USBH_HandleTypeDef *phost) {
   HID_TypeTypeDef hidType;
   HID_KEYBD_Info_TypeDef *keybd_info;
   HID_MOUSE_Info_TypeDef *mouse_info;
@@ -223,10 +418,11 @@ void USBH_HID_EventCallback(USBH_HandleTypeDef *phost) {
   Test_syoch_01(phost);
 }
 
-int _write(int file, char *ptr, int len) {
+extern "C" int _write(int file, char *ptr, int len) {
   HAL_UART_Transmit(&huart2, (uint8_t *)ptr, len, 10);
   return len;
 }
+
 /* USER CODE END 4 */
 
 /**
--- usb/Core/Src/stm32f4xx_hal_msp.c
+++ usb/Core/Src/stm32f4xx_hal_msp.c
@@ -1,22 +1,22 @@
 
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file         stm32f4xx_hal_msp.c
-  * @brief        This file provides code for the MSP Initialization
-  *               and de-Initialization codes.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file         stm32f4xx_hal_msp.c
+ * @brief        This file provides code for the MSP Initialization
+ *               and de-Initialization codes.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
@@ -59,11 +59,9 @@
 
 /* USER CODE END 0 */
 /**
-  * Initializes the Global MSP.
-  */
-void HAL_MspInit(void)
-{
-
+ * Initializes the Global MSP.
+ */
+void HAL_MspInit(void) {
   /* USER CODE BEGIN MspInit 0 */
 
   /* USER CODE END MspInit 0 */
@@ -79,19 +77,17 @@ void HAL_MspInit(void)
 }
 
 /**
-* @brief UART MSP Initialization
-* This function configures the hardware resources used in this example
-* @param huart: UART handle pointer
-* @retval None
-*/
-void HAL_UART_MspInit(UART_HandleTypeDef* huart)
-{
+ * @brief UART MSP Initialization
+ * This function configures the hardware resources used in this example
+ * @param huart: UART handle pointer
+ * @retval None
+ */
+void HAL_UART_MspInit(UART_HandleTypeDef* huart) {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-  if(huart->Instance==USART2)
-  {
-  /* USER CODE BEGIN USART2_MspInit 0 */
-
-  /* USER CODE END USART2_MspInit 0 */
+  if (huart->Instance == USART2) {
+    /* USER CODE BEGIN USART2_MspInit 0 */
+    printf("HAL_UART_MspInit (UART2)\n");
+    /* USER CODE END USART2_MspInit 0 */
     /* Peripheral clock enable */
     __HAL_RCC_USART2_CLK_ENABLE();
 
@@ -100,34 +96,48 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     PA2     ------> USART2_TX
     PA3     ------> USART2_RX
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
+    GPIO_InitStruct.Pin = GPIO_PIN_2 | GPIO_PIN_3;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /* USER CODE BEGIN USART2_MspInit 1 */
+    /* USER CODE BEGIN USART2_MspInit 1 */
 
-  /* USER CODE END USART2_MspInit 1 */
-
+    /* USER CODE END USART2_MspInit 1 */
+  } else {
+    printf("HAL_UART_MspInit (?)\n");
   }
-
 }
 
 /**
-* @brief UART MSP De-Initialization
-* This function freeze the hardware resources used in this example
-* @param huart: UART handle pointer
-* @retval None
-*/
-void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
-{
-  if(huart->Instance==USART2)
-  {
-  /* USER CODE BEGIN USART2_MspDeInit 0 */
+ * @brief UART MSP De-Initialization
+ * This function freeze the hardware resources used in this example
+ * @param huart: UART handle pointer
+ * @retval None
+ */
+void HAL_UART_MspDeInit(UART_HandleTypeDef* huart) {
+  if (huart->Instance == USART1) {
+    /* USER CODE BEGIN USART1_MspDeInit 0 */
 
-  /* USER CODE END USART2_MspDeInit 0 */
+    /* USER CODE END USART1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_USART1_CLK_DISABLE();
+
+    /**USART1 GPIO Configuration
+    PA9     ------> USART1_TX
+    PA10     ------> USART1_RX
+    */
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_9 | GPIO_PIN_10);
+
+    /* USER CODE BEGIN USART1_MspDeInit 1 */
+
+    /* USER CODE END USART1_MspDeInit 1 */
+  } else if (huart->Instance == USART2) {
+    /* USER CODE BEGIN USART2_MspDeInit 0 */
+
+    /* USER CODE END USART2_MspDeInit 0 */
     /* Peripheral clock disable */
     __HAL_RCC_USART2_CLK_DISABLE();
 
@@ -135,13 +145,12 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
     PA2     ------> USART2_TX
     PA3     ------> USART2_RX
     */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_2|GPIO_PIN_3);
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_2 | GPIO_PIN_3);
 
-  /* USER CODE BEGIN USART2_MspDeInit 1 */
+    /* USER CODE BEGIN USART2_MspDeInit 1 */
 
-  /* USER CODE END USART2_MspDeInit 1 */
+    /* USER CODE END USART2_MspDeInit 1 */
   }
-
 }
 
 /* USER CODE BEGIN 1 */
