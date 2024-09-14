61d1831d80ea99487ae40c5527dc3352a3ec5349
syoch <syoch64@gmail.com>
2024-09-14 05:12:44 +0000

    Update(robo3): power up

--- robot-collect/src/app/app.hpp
+++ robot-collect/src/app/app.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-#include <robotics/node/BD621x.hpp>
+#include <robotics/node/BD621xFull.hpp>
+#include <robotics/platform/dout.mbed.hpp>
 
 namespace nhk2024b::robot3 {
 class Actuators {
@@ -16,26 +17,26 @@ class Actuators {
     PinName arm_extension_motor_rin;
   };
 
-  robotics::node::BD621x move_motor_l;
-  robotics::node::BD621x move_motor_r;
-  robotics::node::BD621x arm_elevation_motor;
-  robotics::node::BD621x arm_extension_motor;
+  robotics::node::BD621xFull move_motor_l;
+  robotics::node::BD621xFull move_motor_r;
+  robotics::node::BD621xFull arm_elevation_motor;
+  robotics::node::BD621xFull arm_extension_motor;
 
   Actuators(const Config& config)
-      : move_motor_l{std::make_shared<robotics::driver::PWM>(
+      : move_motor_l{std::make_shared<robotics::driver::Dout>(
                          config.move_motor_l_fin),
-                     std::make_shared<robotics::driver::PWM>(
+                     std::make_shared<robotics::driver::Dout>(
                          config.move_motor_l_rin)},
         move_motor_r{
-            std::make_shared<robotics::driver::PWM>(config.move_motor_r_fin),
-            std::make_shared<robotics::driver::PWM>(config.move_motor_r_rin)},
-        arm_elevation_motor{std::make_shared<robotics::driver::PWM>(
+            std::make_shared<robotics::driver::Dout>(config.move_motor_r_fin),
+            std::make_shared<robotics::driver::Dout>(config.move_motor_r_rin)},
+        arm_elevation_motor{std::make_shared<robotics::driver::Dout>(
                                 config.arm_elevation_motor_fin),
-                            std::make_shared<robotics::driver::PWM>(
+                            std::make_shared<robotics::driver::Dout>(
                                 config.arm_elevation_motor_rin)},
-        arm_extension_motor{std::make_shared<robotics::driver::PWM>(
+        arm_extension_motor{std::make_shared<robotics::driver::Dout>(
                                 config.arm_extension_motor_fin),
-                            std::make_shared<robotics::driver::PWM>(
+                            std::make_shared<robotics::driver::Dout>(
                                 config.arm_extension_motor_rin)} {}
 };
 }  // namespace nhk2024b::robot3
