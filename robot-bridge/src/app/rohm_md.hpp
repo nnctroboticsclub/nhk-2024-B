242988536663d19c47296ff33deeb7a3160a1d39
syoch <syoch64@gmail.com>
2024-09-16 02:29:21 +0000

    Update: Reduce smoe nodes for heap size

--- robot-bridge/src/app/rohm_md.hpp
+++ robot-bridge/src/app/rohm_md.hpp
@@ -6,14 +6,14 @@
 #include <robotics/node/node.hpp>
 
 namespace nhk2024b::common {
-class aRohm1chMD {
+class Rohm1chMD {
   ::RohmMD md;
 
  public:
   robotics::Node<float> in_velocity;
-  robotics::Node<float> out_velocity;  // [rad/s]
-  robotics::Node<float> out_current;   // [A]
-  robotics::Node<float> out_radian;    // [rad]
+  // robotics::Node<float> out_velocity;  // [rad/s]
+  // robotics::Node<float> out_current;   // [A]
+  // robotics::Node<float> out_radian;    // [rad]
 
   Rohm1chMD(ikarashiCAN_mk2 &can, int id) : md(&can, id) {
     in_velocity.SetChangeCallback([this](float v) { md.set(0, v); });
@@ -23,9 +23,9 @@ class aRohm1chMD {
 
   bool Read() {
     auto r = md.read();
-    out_velocity.SetValue(md.get_vel());
-    out_current.SetValue(md.get_cur());
-    out_radian.SetValue(md.get_rad());
+    // out_velocity.SetValue(md.get_vel());
+    // out_current.SetValue(md.get_cur());
+    // out_radian.SetValue(md.get_rad());
 
     return r;
   }
