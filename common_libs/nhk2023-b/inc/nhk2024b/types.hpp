7a5148bb1574a48afee3d095f3e9d82af75339fa
syoch <syoch64@gmail.com>
2024-09-20 14:25:36 +0000

    Update(common lib): Added Muxer shortcut

--- common_libs/nhk2023-b/inc/nhk2024b/types.hpp
+++ common_libs/nhk2023-b/inc/nhk2024b/types.hpp
@@ -3,6 +3,7 @@
 #include <robotics/node/node.hpp>
 #include <robotics/types/joystick_2d.hpp>
 #include <robotics/filter/angled_motor.hpp>
+#include <robotics/filter/muxer.hpp>
 #include <robotics/logger/logger.hpp>
 
 namespace nhk2024b {
@@ -10,4 +11,7 @@ template <typename T>
 using Node = robotics::Node<T>;
 using JoyStick2D = robotics::JoyStick2D;
 extern robotics::logger::Logger logger;
+
+template <typename T>
+using Muxer = robotics::filter::Muxer<T>;
 }  // namespace nhk2024b
